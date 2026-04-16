7cb5a8cf1b9c78558917a0ec851bf107d81cf7fe
omar.alsayed <o.alsayed@penta-b.com>
2026-04-16 23:10:57 +0200

    reel section commented for now

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <SectionRenderer>
-        <ReelCarousel />
+        {/* <ReelCarousel /> */}
         <StatsBar />
         <WorksPreview />
         <BrandsMarquee />
